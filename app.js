924836c99cc378ee04a09277a36fdcf6af25ce7a
FA Rohan <mdr711780@gmail.com>
2026-06-27 07:46:50 +0600

    Update app.js

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 app.use(express.json());
 
-app.use(session({
-    secret: 's567er6ryfhjfgjuyfkujti785789tihi978990yioghpersecreto87toiluglkrt7io8rfourti785rey',
-    resave: false,
-    saveUninitilized: true,
-    cookie: {secure:false}
-}));
+app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: false, 
+  saveUninitialized: true,
+  cookie: { 
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true 
+  } 
 
 app.use("/api/auth", authRoutes);
 
 connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`.bgBlue);
 });
